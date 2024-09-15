c4a4066d8e915610a717588a3f80beaf9328b442
hienhuynhdang <hienhuynh406@icloud.com>
2024-09-16 03:15:58 +0700

    fix: update cmd run nillion

--- infra/modules/ecs/nillion/task_definition.tf
+++ infra/modules/ecs/nillion/task_definition.tf
@@ -3,8 +3,8 @@ resource "aws_ecs_task_definition" "core" {
   family                   = aws_ecs_cluster.cluster.name
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 2048
+  memory                   = 4096
   container_definitions = jsonencode([
     {
       name  = "plat-fellowship-${var.environment_name}-nillion"
