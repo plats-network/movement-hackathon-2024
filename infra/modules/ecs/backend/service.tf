dca8fd4f3c8dd1ab7a7807134447452b586c8058
hienhuynhdang <hienhuynh406@icloud.com>
2024-09-15 16:35:40 +0700

    fix: add env config for backend

--- infra/modules/ecs/backend/service.tf
+++ infra/modules/ecs/backend/service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "service" {
   name            = "plat-fellowship-${var.environment_name}-backend"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.core.arn
-  desired_count   = 0
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   load_balancer {
--- infra/modules/ecs/backend/task_definition.tf
+++ infra/modules/ecs/backend/task_definition.tf
@@ -22,18 +22,117 @@ resource "aws_ecs_task_definition" "core" {
       command    = []
       environment = [
         {
-          "name" : "FOO",
-          "value" : "bar"
+          "name" : "MODE",
+          "value" : "local"
         },
-
+        {
+          "name" : "HOST",
+          "value" : "0.0.0.0"
+        },
+        {
+          "name" : "PORT",
+          "value" : "80"
+        },
+        {
+          "name" : "RELOAD",
+          "value" : "False"
+        },
+        {
+          "name" : "MONGO_DB",
+          "value" : "plat"
+        },
+        {
+          "name" : "MONGO_USER_COLLECTION",
+          "value" : "users"
+        },
+        {
+          "name" : "ALGORITHM",
+          "value" : "HS256"
+        },
+        {
+          "name" : "ACCESS_TOKEN_EXPIRE_MINUTES",
+          "value" : "60"
+        }
       ]
       environmentFiles = []
       mountPoints      = []
       volumesFrom      = []
       secrets = [
         {
-          name      = "FOOO",
-          valueFrom = "${var.secret_arn}:bar::"
+          name      = "MONGO_HOST",
+          valueFrom = "${var.secret_arn}:mongoHost::"
+        },
+        {
+          name      = "MONGO_PORT",
+          valueFrom = "${var.secret_arn}:mongoPort::"
+        },
+        {
+          name      = "MONGO_USER",
+          valueFrom = "${var.secret_arn}:mongoUser::"
+        },
+        {
+          name      = "MONGO_PASSWORD",
+          valueFrom = "${var.secret_arn}:mongoPassword::"
+        },
+        {
+          name      = "MONGO_URI",
+          valueFrom = "${var.secret_arn}:mongoUri::"
+        },
+        {
+          name      = "REDIS_HOST",
+          valueFrom = "${var.secret_arn}:redisHost::"
+        },
+        {
+          name      = "REDIS_PORT",
+          valueFrom = "${var.secret_arn}:redisPort::"
+        },
+        {
+          name      = "REDIS_PASSWORD",
+          valueFrom = "${var.secret_arn}:redisPassword::"
+        },
+        {
+          name      = "REDIS_BROKER_URL",
+          valueFrom = "${var.secret_arn}:redisBrokerUrl::"
+        },
+        {
+          name      = "SECRET_KEY",
+          valueFrom = "${var.secret_arn}:secretKey::"
+        },
+        {
+          name      = "NILLION_CLUSTER_ID",
+          valueFrom = "${var.secret_arn}:nillionClusterId::"
+        },
+        {
+          name      = "NILLION_BOOTNODE_MULTIADDRESS",
+          valueFrom = "${var.secret_arn}:nillionBootnodeMultiaddress::"
+        },
+        {
+          name      = "NILLION_BOOTNODE_WEBSOCKET",
+          valueFrom = "${var.secret_arn}:nillionBootnodeWebsocket::"
+        },
+        {
+          name      = "NILLION_SEED",
+          valueFrom = "${var.secret_arn}:nillionSeed::"
+        },
+        {
+          name      = "NILLION_NILCHAIN_CHAIN_ID",
+          valueFrom = "${var.secret_arn}:nillionNilchainChainId::"
+        },
+        {
+          name      = "NILLION_NILCHAIN_JSON_RPC",
+          valueFrom = "${var.secret_arn}:nillionNilchainJsonRpc::"
+        },
+        {
+          name      = "NILLION_NILCHAIN_REST_API",
+          valueFrom = "${var.secret_arn}:nillionNilchainRestApi::"
+        },
+        {
+          name      = "NILLION_NILCHAIN_GRPC",
+          valueFrom = "${var.secret_arn}:nillionNilchainGrpc::"
+        },
+        {
+          name      = "NILLION_NILCHAIN_PRIVATE_KEY_0",
+          valueFrom = "${var.secret_arn}:nillionNilchainPrivateKey0::"
         }
       ]
       dnsServers            = []
