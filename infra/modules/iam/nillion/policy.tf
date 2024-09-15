3e47ab4c0bc95c775d96dd31389bd6374c946a4c
hienhuynhdang <hienhuynh406@icloud.com>
2024-09-16 01:43:08 +0700

    fix: update cmd run nillion

--- infra/modules/iam/nillion/policy.tf
+++ infra/modules/iam/nillion/policy.tf
@@ -35,7 +35,14 @@ resource "aws_iam_policy" "ecs_task_container_policy" {
   name = "plat-fellowship-${var.environment_name}-nillion-ecs-task-con"
 
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = []
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["secretsmanager:GetSecretValue"]
+        Effect   = "Allow"
+        Resource = "*"
+        Sid      = "SecretManager"
+      }
+    ]
   })
 }
