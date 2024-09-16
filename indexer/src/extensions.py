cf19a2598cbf070fb602952f9883fdd46a8613e3
hienhuynhdang <hienhuynh406@icloud.com>
2024-09-16 20:38:49 +0700

    remove db

--- indexer/src/extensions.py
+++ indexer/src/extensions.py
@@ -4,7 +4,7 @@ from src.config import BaseConfig as Conf
 pool = redis.ConnectionPool(
     host=Conf.REDIS_HOST,
     port=Conf.REDIS_PORT,
-    db=Conf.REDIS_DB,
+    # db=Conf.REDIS_DB,
     password=Conf.REDIS_PASSWORD,
     retry_on_timeout=True,
     health_check_interval=10
