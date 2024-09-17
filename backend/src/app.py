89aa884decfd69b2369ce85d5adab745bcc8660b
uKaxg <uka.pgr@gmail.com>
2024-09-17 16:53:25 +0700

    Refactor CORS middleware in app.py to remove commented out code and allow all origins

--- backend/src/app.py
+++ backend/src/app.py
@@ -17,15 +17,15 @@ def create_app(config=None, app_name=None):
         openapi_url=f"{settings.API_V1_STR}/openapi.json"
     )
 
-    origins = ["http://localhost:3000"]
-    # Add CORS
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # origins = ["http://localhost:3000"]
+    # # Add CORS
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=origins,
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
     
     # Add middleware
     # app.add_middleware(CustomHeadersMiddleware)
