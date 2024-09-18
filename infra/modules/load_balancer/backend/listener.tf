631f77d4cb0504980a411247708e7ef7ec84c9c1
hienhuynhdang <danghuynhphuc_hien@ca-adv.co.jp>
2024-09-18 13:04:58 +0700

    fix: allow ip aiden

--- infra/modules/load_balancer/backend/listener.tf
+++ infra/modules/load_balancer/backend/listener.tf
@@ -68,8 +68,9 @@ resource "aws_lb_listener_rule" "internal_api_allow" {
     source_ip {
       values = [
         var.vpc_cidr_block,
-        "171.252.188.6/32", # Aiden Sepiol home IP
-        "222.253.79.150/32" # Odin Hoang home IP
+        "171.252.188.6/32",  # Aiden Sepiol home IP
+        "222.253.79.150/32", # Odin Hoang home IP
+        "118.155.249.8/32"   # Aiden Sepiol c IP
       ]
     }
   }
