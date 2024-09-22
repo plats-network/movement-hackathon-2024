27dbddf12040f93a19eda4c906a8e1b6655c9b8f
= <=>
2024-09-22 16:36:21 +0700

    chore: fix responsive

--- frontend/src/components/ProfileView/YourUnifiedIdProfile.tsx
+++ frontend/src/components/ProfileView/YourUnifiedIdProfile.tsx
@@ -1,7 +1,7 @@
 import EthereumIcon from "@/assets/EthereumIcon";
 import React from "react";
 
-const YourUnifiedIdProfile = ({data}: {data: any}) => {
+const YourUnifiedIdProfile = ({ data }: { data: any }) => {
   return (
     <div className="relative w-full ">
       <div
@@ -19,31 +19,35 @@ const YourUnifiedIdProfile = ({data}: {data: any}) => {
 
           <EthereumIcon />
         </div>
-        <div className="flex gap-2  max-w-[667px] w-full">
-          <div className="group gradient-border-mask  max-w-[251px] w-full  flex flex-col gap-2 text-center py-3 px-8 bg-[#1A1A36] hover:bg-[#060625] cursor-pointer rounded-[12px]">
+        <div className="flex sm:flex-row flex-col gap-2  max-w-[667px] w-full">
+          <div className="group gradient-border-mask  md:w-[200px]  w-full  flex flex-col gap-2 text-center py-3  bg-[#1A1A36] hover:bg-[#060625] cursor-pointer rounded-[12px]">
             <p className="text-[40px] font-bold group-hover:text-[#3AE7E7]">
-            {Number(data?.balance) >= 0 ? data?.balance : "- -"}
+              {Number(data?.balance) >= 0
+                ? Number(data?.balance).toFixed(1)
+                : "- -"}
             </p>
             <p className="text-[17px] text-[#B7B4BB] group-hover:text-white">
               Your balance
             </p>
           </div>
-          <div className="group gradient-border-mask max-w-[251px] w-full flex flex-col gap-2 text-center py-3 px-8 bg-[#1A1A36] hover:bg-[#060625] cursor-pointer rounded-[12px]">
+          <div className="group gradient-border-mask md:w-[200px]  w-full flex flex-col gap-2 text-center py-3  bg-[#1A1A36] hover:bg-[#060625] cursor-pointer rounded-[12px]">
             <p className="text-[40px] font-bold group-hover:text-[#3AE7E7]">
-            {Number(data?.volume) >= 0 ? data?.volume : "- -"}
-
+              {Number(data?.volume) >= 0
+                ? Number(data?.volume).toFixed(1)
+                : "- -"}
             </p>
             <p className="text-[17px] text-[#B7B4BB] group-hover:text-white">
-            One month volume
+              One month volume
             </p>
           </div>
-          <div className="group gradient-border-mask  max-w-[251px] w-full  flex flex-col gap-2 text-center py-3 px-8 bg-[#1A1A36] hover:bg-[#060625] cursor-pointer rounded-[12px]">
+          <div className="group gradient-border-mask  md:w-[200px] w-full  flex flex-col gap-2 text-center py-3  bg-[#1A1A36] hover:bg-[#060625] cursor-pointer rounded-[12px]">
             <p className="text-[40px] font-bold group-hover:text-[#3AE7E7]">
-            {Number(data?.twitter) >= 0 ? data?.twitter : "- -"}
-
+              {Number(data?.twitter) >= 0
+                ? Number(data?.twitter).toFixed(1)
+                : "- -"}
             </p>
             <p className="text-[17px] text-[#B7B4BB] group-hover:text-white">
-            Twitter Scores
+              Twitter Scores
             </p>
           </div>
         </div>
