c10ac790bdea39f0168d69a596352e73f0ee8e9d
CocDap <hodinhdung1997@gmail.com>
2024-09-23 15:04:19 +0700

    feat: update contract with multiple accounts with unique id

--- contract/tests/contract.ts
+++ contract/tests/contract.ts
@@ -12,25 +12,15 @@ describe("Plats Id", () => {
   const program = anchor.workspace.PlatsId as Program<PlatsId>;
   const signer = provider.wallet as NodeWallet;
   const users = Array.from({ length: 10 }, () => anchor.web3.Keypair.generate());
-  const owner = users[0].publicKey;
+  const master_owner = users[0].publicKey;
 
-  const owner1 = users[1].publicKey;
-  const owner2 = users[2].publicKey;
-  console.log("Owner:",owner2);
-  const owner3 = users[3].publicKey;
-  const owner4 = users[4].publicKey;
-  // let [identityManagementPDA, _] = PublicKey.findProgramAddressSync(
-  //   [
-  //     anchor.utils.bytes.utf8.encode('identity_management'),
-  //     owner4.toBuffer()
-  //   ],
-  //   program.programId
-  // );
-  // console.log("identityManagementPDA:", identityManagementPDA);
+  const slave_owner1 = users[1].publicKey;
+  const slave_owner2 = users[2].publicKey;
+
     let [identityPDA, bump] = PublicKey.findProgramAddressSync(
       [
         anchor.utils.bytes.utf8.encode('identity'),
-        owner2.toBuffer()
+        Buffer.from("Linh.ID")
       ],
       program.programId
     );
@@ -41,13 +31,13 @@ describe("Plats Id", () => {
   it("Register Identity", async () => {
 
 
-
-    let nameId = "BOB.ID";
+    // Master owner register unique id 
+    let nameId = "Linh.ID";
     let storeIdBalance = "c999d3e9-a6c1-438b-a121-295115728f01";
     let secretNameBalance = "secret_balance";
 
     let storeIdVolume = "c999d3e9-a6c1-438b-a121-295115728f01";
-    let secretNameVolume = "secret_volume";
+    let secretNameVolume = "secret_volumn";
 
     let storeIdTwitter = "c999d3e9-a6c1-438b-a121-295115728f01";
     let secretNameTwitter = "secret_twitter";
@@ -56,7 +46,7 @@ describe("Plats Id", () => {
       .registerIdentity(nameId, [storeIdBalance, storeIdVolume,storeIdTwitter ], [secretNameBalance, secretNameVolume, secretNameTwitter], 1)
     .accounts({
       identity: identityPDA,
-      owner: owner2
+      masterOwner: master_owner
     })
     .rpc();
   });
@@ -68,20 +58,22 @@ describe("Plats Id", () => {
     const accountData = await program.account.identity.fetch(identityPDA);
 
     console.log(`Name : ${accountData.nameId}`);
-    console.log(`Profiles: ${JSON.stringify(accountData.infos)}`);
-    console.log(`Owners: ${JSON.stringify(accountData.owner)}`);
+    console.log(`Slave Accounts: ${JSON.stringify(accountData.slaveAccounts)}`);
+    console.log(`Balance: ${JSON.stringify(accountData.balancePrivacy)}`);
+    console.log(`Volumn: ${JSON.stringify(accountData.volumnPrivacy)}`);
+    console.log(`Twitter: ${JSON.stringify(accountData.twitterPrivacy)}`);
   });
 
 
   it("Update  Identity", async () => {
 
 
-    let nameId = "BOB.ID";
+    let nameId = "Linh.ID";
     let storeIdBalance = "c888d3e9-a6c1-438b-a121-295115728f01";
     let secretNameBalance = "secret_balance";
 
     let storeIdVolume = "c888d3e9-a6c1-438b-a121-295115728f01";
-    let secretNameVolume = "secret_volume";
+    let secretNameVolume = "secret_volumn";
 
     let storeIdTwitter = "c888d3e9-a6c1-438b-a121-295115728f01";
     let secretNameTwitter = "secret_twitter";
@@ -90,11 +82,129 @@ describe("Plats Id", () => {
       .updateIdentity(nameId, [storeIdBalance, storeIdVolume,storeIdTwitter ], [secretNameBalance, secretNameVolume, secretNameTwitter])
     .accounts({
       identity: identityPDA,
-      owner: owner2
+      account: master_owner
     })
     .rpc();
   });
 
+  it("Get Identity After", async () => {
+
+
+
+    const accountData = await program.account.identity.fetch(identityPDA);
+
+    console.log(`Name : ${accountData.nameId}`);
+    console.log(`Slave Accounts: ${JSON.stringify(accountData.slaveAccounts)}`);
+    console.log(`Balance: ${JSON.stringify(accountData.balancePrivacy)}`);
+    console.log(`Volumn: ${JSON.stringify(accountData.volumnPrivacy)}`);
+    console.log(`Twitter: ${JSON.stringify(accountData.twitterPrivacy)}`);
+  });
+
+  it("Add new slave account 1 to unique  Identity ", async () => {
+
+
+    let nameId = "Linh.ID";
+    let storeIdBalance = "c777d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameBalance = "secret_balance";
+
+    let storeIdVolume = "c777d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameVolume = "secret_volumn";
+
+    let storeIdTwitter = "c777d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameTwitter = "secret_twitter";
+
+    const transactionSignature = await program.methods
+      .addIdentity(nameId, [storeIdBalance, storeIdVolume,storeIdTwitter ], [secretNameBalance, secretNameVolume, secretNameTwitter])
+    .accounts({
+      identity: identityPDA,
+      account: slave_owner1
+    })
+    .rpc();
+  });
+
+  it("Get Identity After Adding new slave account 1", async () => {
+
+
+
+    const accountData = await program.account.identity.fetch(identityPDA);
+
+    console.log(`Name : ${accountData.nameId}`);
+    console.log(`Slave Accounts: ${JSON.stringify(accountData.slaveAccounts)}`);
+    console.log(`Balance: ${JSON.stringify(accountData.balancePrivacy)}`);
+    console.log(`Volumn: ${JSON.stringify(accountData.volumnPrivacy)}`);
+    console.log(`Twitter: ${JSON.stringify(accountData.twitterPrivacy)}`);
+  });
+
+
+  it("Add new slave account 2 to unique  Identity ", async () => {
+
+
+    let nameId = "Linh.ID";
+    let storeIdBalance = "c666d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameBalance = "secret_balance";
+
+    let storeIdVolume = "c666d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameVolume = "secret_volumn";
+
+    let storeIdTwitter = "c666d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameTwitter = "secret_twitter";
+
+    const transactionSignature = await program.methods
+      .addIdentity(nameId, [storeIdBalance, storeIdVolume,storeIdTwitter ], [secretNameBalance, secretNameVolume, secretNameTwitter])
+    .accounts({
+      identity: identityPDA,
+      account: slave_owner2
+    })
+    .rpc();
+  });
+
+  it("Get Identity After Adding new slave account 2", async () => {
+
+
+
+    const accountData = await program.account.identity.fetch(identityPDA);
+
+    console.log(`Name : ${accountData.nameId}`);
+    console.log(`Slave Accounts: ${JSON.stringify(accountData.slaveAccounts)}`);
+    console.log(`Balance: ${JSON.stringify(accountData.balancePrivacy)}`);
+    console.log(`Volumn: ${JSON.stringify(accountData.volumnPrivacy)}`);
+    console.log(`Twitter: ${JSON.stringify(accountData.twitterPrivacy)}`);
+  });
+
+  it("Update  Identity for slave account 1", async () => {
+
+
+    let nameId = "Linh.ID";
+    let storeIdBalance = "c555d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameBalance = "secret_balance";
+
+    let storeIdVolume = "c555d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameVolume = "secret_volumn";
+
+    let storeIdTwitter = "c555d3e9-a6c1-438b-a121-295115728f01";
+    let secretNameTwitter = "secret_twitter";
+
+    const transactionSignature = await program.methods
+      .updateIdentity(nameId, [storeIdBalance, storeIdVolume,storeIdTwitter ], [secretNameBalance, secretNameVolume, secretNameTwitter])
+    .accounts({
+      identity: identityPDA,
+      account: slave_owner1
+    })
+    .rpc();
+  });
+
+  it("Get Identity After slave account 1 updated", async () => {
+
+
+
+    const accountData = await program.account.identity.fetch(identityPDA);
+
+    console.log(`Name : ${accountData.nameId}`);
+    console.log(`Slave Accounts: ${JSON.stringify(accountData.slaveAccounts)}`);
+    console.log(`Balance: ${JSON.stringify(accountData.balancePrivacy)}`);
+    console.log(`Volumn: ${JSON.stringify(accountData.volumnPrivacy)}`);
+    console.log(`Twitter: ${JSON.stringify(accountData.twitterPrivacy)}`);
+  });
 
 
 
