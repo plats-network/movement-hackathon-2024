c83587be01114cde5bafdb8d251fe153f367e05e
uKaxg <uka.pgr@gmail.com>
2024-09-23 10:40:24 +0700

    Refactor API endpoints to remove unused options requests and update CORS headers

--- backend/src/middleware/options.py
+++ backend/src/middleware/options.py
@@ -5,7 +5,12 @@ from starlette.responses import Response
 class OptionsMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         if request.method == "OPTIONS":
-            return Response(status_code=200)
+            response = Response(status_code=200)
+            response.headers['Access-Control-Allow-Origin'] = "*"
+            response.headers['Access-Control-Allow-Credentials'] = 'false'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS, DELETE'
+            response.headers['Access-Control-Allow-Headers'] = "*"
+            return response
         response = await call_next(request)
         response.headers['Access-Control-Allow-Origin'] = "*"
         response.headers['Access-Control-Allow-Credentials'] = 'false'
