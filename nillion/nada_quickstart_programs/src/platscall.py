3c54aac557f3e75768abef988ac3d66e43de0b91
CocDap <48429826+CocDap@users.noreply.github.com>
2024-09-24 22:34:43 +0700

    add score nada (#43)
    * feat: fix bug
    
    * fix bug
    
    * feat: add score

--- nillion/nada_quickstart_programs/src/platscall.py
+++ nillion/nada_quickstart_programs/src/platscall.py
@@ -13,6 +13,14 @@ def nada_main():
 
     twitter = SecretInteger(Input(name="secret_twitter", party=party_plats))
 
+
+    # score formula
+    balance_multiplier = balance * Integer(2)
+    volume_multiplier = volume * Integer(3) 
+    score = (balance_multiplier + volume_multiplier + twitter) / Integer(6)
+
+    id.append(Output(score, "result_score", party_plats))
+
     threshold_trade = SecretInteger(Input(name="threshold_trade", party=party_plats))
     threshold_whale = SecretInteger(Input(name="threshold_whale", party=party_plats))
     threshold_kol = SecretInteger(Input(name="threshold_kol", party=party_plats))
