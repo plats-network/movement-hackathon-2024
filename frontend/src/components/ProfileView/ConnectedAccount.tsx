35f26778b3e3fb8f2ac41de1d0f154d2444225a3
= <=>
2024-09-26 20:50:05 +0700

    chore: fix show account

--- frontend/src/components/ProfileView/ConnectedAccount.tsx
+++ frontend/src/components/ProfileView/ConnectedAccount.tsx
@@ -24,12 +24,14 @@ const ConnectedAccount = ({
             "linear-gradient(90deg, rgba(25,5,50,1),rgba(102,255,255,1) 0%)",
         }}
       ></div>
-      <div className="relative z-10 h-full flex flex-col justify-between gap-8 p-5 ">
+      <div className="relative z-10 h-full flex flex-col  gap-8 p-5 ">
         <p className="text-xl text-[#3AE7E7] font-semibold">
           Connected Account
         </p>
         {user?.twitter_name || user?.address ? (
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 h-[260px] overflow-y-scroll " style={{
+            scrollbarWidth:'none'
+          }}>
             {user?.address &&
               user?.address.map((address: string, index: number) => (
                 <div
@@ -51,34 +53,9 @@ const ConnectedAccount = ({
                 </div>
               ))}
 
-            {/* <div className="bg-[#1E2536] flex items-center justify-between py-[18px] px-[10px] gap-[20px] rounded-[8px] box-gradient-border-mask">
-          <div className="flex items-center gap-2">
-            <GoogleIcon />
-            <p>@dustinhuynh</p>
-          </div>
-  
-          <div className="flex gap-3">
-            <label className="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="relative w-8 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 rounded-full peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[4px] after:end-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-r from-[#8737E9] to-[#3AE7E7]"></div>
-            </label>
-            <DeleteIcon />
-          </div>
-        </div> */}
-            {/* <div className="bg-[#1E2536] flex items-center justify-between py-[18px] px-[10px] gap-[20px] rounded-[8px] box-gradient-border-mask">
-          <div className="flex items-center gap-2">
-            <TelegramIcon />
-            <p>@dustinhuynh</p>
-          </div>
-  
-          <div className="flex gap-3">
-            <label className="inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="relative w-8 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 rounded-full peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[4px] after:end-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-r from-[#8737E9] to-[#3AE7E7]"></div>
-            </label>
-            <DeleteIcon />
-          </div>
-        </div> */}
+       
+
+     
 
             {
               //  user?.twitter_name !== -1 || user?.twitter_name
@@ -101,7 +78,7 @@ const ConnectedAccount = ({
             }
           </div>
         ) : (
-          <div className="flex flex-col items-center justify-center text-center gap-3">
+          <div className="flex flex-col items-center justify-center text-center gap-3 h-[260px]">
             <p className="text-[40px] font-bold">- -</p>
             <p className="text-[17px] text-[#B172FF]">
               You haven't connected to any accounts yet
